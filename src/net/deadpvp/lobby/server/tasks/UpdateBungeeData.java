5288c39f1fa4a425cb83a64c8e5fcf18533e198c
Red_Spash <red.spash.game@gmail.com>
2023-12-12 21:46:10 +0100

    Renamed file

--- src/net/deadpvp/lobby/server/tasks/UpdateBungeeData.java
+++ src/net/deadpvp/lobby/server/tasks/UpdateBungeeData.java
@@ -3,16 +3,17 @@ package net.deadpvp.lobby.server.tasks;
 import net.deadpvp.lobby.server.BungeeManager;
 import org.bukkit.scheduler.BukkitRunnable;
 
-public class UpdatePlayerCount extends BukkitRunnable {
+public class UpdateBungeeData extends BukkitRunnable {
 
     private final BungeeManager bungeeManager;
 
-    public UpdatePlayerCount(BungeeManager bungeeManager) {
+    public UpdateBungeeData(BungeeManager bungeeManager) {
         this.bungeeManager = bungeeManager;
     }
 
     @Override
     public void run() {
         this.bungeeManager.updatePlayerCount();
+        this.bungeeManager.updateServersStatus();
     }
 }
